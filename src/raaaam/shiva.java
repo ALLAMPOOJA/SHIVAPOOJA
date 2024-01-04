e99039538ca69a79259aeaace99ad4ef00ac142d
POOJA <poojaaallam@gmail.com>
2024-01-04 16:25:58 +0530

    hi commit

---
+++ src/raaaam/shiva.java
@@ -0,0 +1,13 @@
+package raaaam;
+
+public class shiva {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Poooja tells hi to Shiva");
+		System.out.println("But shiva rejected that greeting and said bye");
+
+	}
+
+}
